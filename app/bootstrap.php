71d0e23ed49b32ca70efb1ecea0c6275c2f01f7a
Jaroslav Tyc <jaroslav.tyc@expensa.cz>
2014-12-19 15:05:58 +0100

    fix: Race and gender enum types are recognized

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,5 +3,5 @@ require_once __DIR__ . "/autoload.php";
 
 use Doctrine\DBAL\Types\Type;
 
-Type::addType(\DrdPlus\Cave\UnitBundle\Enum\Races\RaceEnumType::TYPE, \Doctrineum\EnumType::class);
-Type::addType(\DrdPlus\Cave\UnitBundle\Enum\Races\Genders\GenderEnumType::TYPE, \Doctrineum\EnumType::class);
+Type::addType(\DrdPlus\Cave\UnitBundle\Enum\Races\RaceEnumType::TYPE, \DrdPlus\Cave\UnitBundle\Enum\Races\RaceEnumType::class);
+Type::addType(\DrdPlus\Cave\UnitBundle\Enum\Races\Genders\GenderEnumType::TYPE, \DrdPlus\Cave\UnitBundle\Enum\Races\Genders\GenderEnumType::class);
