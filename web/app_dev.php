890430cc7deb91156780d22d29299491c37b6c30
Jaroslav Tyc <jaroslav.tyc@expensa.cz>
2014-12-19 15:55:47 +0100

    refactoring: boostrap file renamed

--- web/app_dev.php
+++ web/app_dev.php
@@ -21,7 +21,7 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
 
 require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__ . '/../app/bootstrap.php';
+require_once __DIR__ . '/../app/_bootstrap.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
