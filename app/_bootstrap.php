60e26847795f4a8c4e083be9775c7b58ad670c9f
Jaroslav Tyc <jaroslav.tyc@expensa.cz>
2014-12-30 16:58:04 +0100

    refactoring: Registering of race and gender enums slightly clarified.

--- app/_bootstrap.php
+++ app/_bootstrap.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . "/autoload.php";
 
 use Doctrine\DBAL\Types\Type;
+use \DrdPlus\Cave\UnitBundle\Enum\Races\RaceEnumType;
+use \DrdPlus\Cave\UnitBundle\Enum\Races\GenderEnumType;
 
-Type::addType(\DrdPlus\Cave\UnitBundle\Enum\Races\RaceEnumType::TYPE, \DrdPlus\Cave\UnitBundle\Enum\Races\RaceEnumType::class);
-Type::addType(\DrdPlus\Cave\UnitBundle\Enum\Races\GenderEnumType::TYPE, \DrdPlus\Cave\UnitBundle\Enum\Races\GenderEnumType::class);
+Type::addType(RaceEnumType::TYPE, RaceEnumType::class);
+Type::addType(GenderEnumType::TYPE, GenderEnumType::class);
