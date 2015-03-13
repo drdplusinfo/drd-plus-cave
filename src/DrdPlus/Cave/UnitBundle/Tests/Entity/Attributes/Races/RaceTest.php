43283fd4eb9fbf713230c169f927a838fdc308eb
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-03-13 08:42:14 +0100

    Race has to be concrete class

--- src/DrdPlus/Cave/UnitBundle/Tests/Entity/Attributes/Races/RaceTest.php
+++ src/DrdPlus/Cave/UnitBundle/Tests/Entity/Attributes/Races/RaceTest.php
@@ -2,17 +2,10 @@
 namespace DrdPlus\Cave\UnitBundle\Entity\Attributes\Races;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
 
-class RaceTest extends \PHPUnit_Framework_TestCase {
-
-    /**
-     * @test
-     */
-    public function can_register_self_even_as_abstract_class()
-    {
-        Race::registerSelf();
-        $this->assertTrue(Race::hasType(Race::getTypeName()));
-    }
+class RaceTest extends \PHPUnit_Framework_TestCase
+{
 
     /**
      * @test
@@ -25,9 +18,19 @@ class RaceTest extends \PHPUnit_Framework_TestCase {
 
     /**
      * @test
+     * @depends has_type_name_as_expected
+     */
+    public function can_register_self()
+    {
+        Race::registerSelf();
+        $this->assertTrue(Race::hasType(Race::getTypeName()));
+    }
+
+    /**
+     * @test
      * @expectedException \DrdPlus\Cave\UnitBundle\Entity\Attributes\Races\Exceptions\AbstractRaceCanNotBeCreated
      */
-    public function creating_abstract_race_enum_cause_exception()
+    public function creating_race_enum_itself_cause_exception()
     {
         Race::getEnum('foo');
     }
@@ -36,13 +39,25 @@ class RaceTest extends \PHPUnit_Framework_TestCase {
      * @test
      * @expectedException \DrdPlus\Cave\UnitBundle\Entity\Attributes\Races\Exceptions\MissingRaceCodeImplementation
      */
-    public function creating_abstract_race_enum_by_shortcut_cause_exception()
+    public function creating_race_enum_by_shortcut_cause_exception()
     {
         Race::getIt();
     }
 
+
     /**
      * @test
+     * @depends can_register_self
+     */
+    public function can_be_created_as_enum_type()
+    {
+        $race = Type::getType(Race::getTypeName());
+        $this->assertInstanceOf(Race::class, $race);
+    }
+
+    /**
+     * @test
+     * @depends can_register_self
      */
     public function subrace_can_be_created()
     {
@@ -53,10 +68,12 @@ class RaceTest extends \PHPUnit_Framework_TestCase {
 
     /**
      * @test
+     * @depends can_register_self
      */
     public function race_returns_proper_subrace()
     {
-        $genericRace = Race::getType(Race::getTypeName());
+        /** @var Race $genericRace */
+        $genericRace = Type::getType(Race::getTypeName());
         $genericRace::addSubTypeEnum(TestSubrace::class, $subTypeRegexp = '~bar~');
         $this->assertRegExp($subTypeRegexp, TestSubrace::getTypeName());
         $subrace = $genericRace->convertToPHPValue(TestSubrace::getTypeName(), $this->getPlatform());
@@ -74,7 +91,8 @@ class RaceTest extends \PHPUnit_Framework_TestCase {
 }
 
 /** inner */
-class TestSubrace extends Race {
+class TestSubrace extends Race
+{
 
     public static function getRaceCode()
     {
