539fabc70ec9f358b185b1399f64dd3630ed773f
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-03-16 20:28:14 +0100

    Skurut covered by tests

---
+++ src/DrdPlus/Cave/UnitBundle/Tests/Entity/Attributes/Races/Orcs/SkurutTest.php
@@ -0,0 +1,163 @@
+<?php
+namespace DrdPlus\Cave\UnitBundle\Entity\Attributes\Races\Orcs;
+
+use Doctrine\DBAL\Types\Type;
+use DrdPlus\Cave\UnitBundle\Entity\Attributes\Races\Race;
+use DrdPlus\Cave\UnitBundle\Tests\Entity\Attributes\Races\AbstractTestOfRace;
+
+class SkurutTest extends AbstractTestOfRace
+{
+
+    /**
+     * @test
+     */
+    public function can_register_self()
+    {
+        $raceClass = $this->getSubraceClass();
+        $raceClass::registerSelf();
+        $this->assertTrue(Type::hasType($raceClass::getTypeName()));
+    }
+
+    /**
+     * @return Race
+     *
+     * @test
+     * @depends can_register_self
+     */
+    public function can_create_self()
+    {
+        return parent::can_create_self();
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function gives_expected_male_strength_modifier(Race $race)
+    {
+        $this->assertSame(1, $race->getStrengthModifier($this->getSubraceMale()));
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function gives_expected_male_agility_modifier(Race $race)
+    {
+        $this->assertSame(1, $race->getAgilityModifier($this->getSubraceMale()));
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function gives_expected_female_agility_modifier(Race $race)
+    {
+        $this->assertSame(1, $race->getAgilityModifier($this->getSubraceFemale()));
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function gives_expected_male_knack_modifier(Race $race)
+    {
+        $this->assertSame(-1, $race->getKnackModifier($this->getSubraceMale()));
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function gives_expected_female_knack_modifier(Race $race)
+    {
+        $this->assertSame(-1, $race->getKnackModifier($this->getSubraceFemale()));
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function gives_expected_female_will_modifier(Race $race)
+    {
+        $this->assertSame(+1, $race->getWillModifier($this->getSubraceFemale()));
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function gives_expected_male_charisma_modifier(Race $race)
+    {
+        $this->assertSame(-2, $race->getCharismaModifier($this->getSubraceMale()));
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function gives_expected_female_charisma_modifier(Race $race)
+    {
+        $this->assertSame(-2, $race->getCharismaModifier($this->getSubraceMale()));
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function gives_expected_male_senses_modifier(Race $race)
+    {
+        $this->assertSame(+1, $race->getSensesModifier($this->getSubraceMale()));
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function gives_expected_female_senses_modifier(Race $race)
+    {
+        $this->assertSame(+1, $race->getSensesModifier($this->getSubraceFemale()));
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function requires_dungeon_master_agreement(Race $race)
+    {
+        $this->assertTrue($race->requiresDungeonMasterAgreement());
+    }
+
+    /**
+     * @param Race $race
+     *
+     * @test
+     * @depends can_create_self
+     */
+    public function has_infravision(Race $race)
+    {
+        $this->assertTrue($race->hasInfravision());
+    }
+}
