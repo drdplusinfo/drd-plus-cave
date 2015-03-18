43b68205e5de383e1c2be1c9a123ccaccc83ad01
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-03-18 17:58:41 +0100

    Level value simplified

--- src/DrdPlus/Cave/UnitBundle/Entity/Attributes/ProfessionLevels/LevelValue.php
+++ src/DrdPlus/Cave/UnitBundle/Entity/Attributes/ProfessionLevels/LevelValue.php
@@ -5,27 +5,5 @@ use Doctrineum\Integer\SelfTypedIntegerEnum;
 
 class LevelValue extends SelfTypedIntegerEnum
 {
-
-    /**
-     * Using own namespace to avoid conflicts with other enums
-     *
-     * @param string $enumValue
-     * @param string $namespace
-     * @return SelfTypedIntegerEnum
-     */
-    public static function getEnum($enumValue, $namespace = __CLASS__)
-    {
-        return parent::getEnum($enumValue, $namespace);
-    }
-
-    /**
-     * Gets the strongly recommended name of this type.
-     * Its used at @see \Doctrine\DBAL\Platforms\AbstractPlatform::getDoctrineTypeComment
-     *
-     * @return string
-     */
-    public static function getTypeName()
-    {
-        return 'level_value';
-    }
+    const TYPE_LEVEL_VALUE = 'level_value';
 }
