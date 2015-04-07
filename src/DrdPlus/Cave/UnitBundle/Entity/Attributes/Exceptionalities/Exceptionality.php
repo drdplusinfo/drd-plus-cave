a77c765fd9069676b6f5f5b9cc99a40a4f633638
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-04-07 18:41:44 +0200

    Exceptionality is entity

--- src/DrdPlus/Cave/UnitBundle/Entity/Attributes/Exceptionalities/Exceptionality.php
+++ src/DrdPlus/Cave/UnitBundle/Entity/Attributes/Exceptionalities/Exceptionality.php
@@ -1,73 +1,69 @@
 <?php
 namespace DrdPlus\Cave\UnitBundle\Entity\Attributes\Exceptionalities;
 
-use Doctrineum\Strict\String\SelfTypedStrictStringEnum;
+use DrdPlus\Cave\UnitBundle\Entity\Attributes\Exceptionalities\Choices\ExceptionalityChoice;
+use DrdPlus\Cave\UnitBundle\Entity\Attributes\Exceptionalities\Kinds\ExceptionalityKind;
+use Granam\Strict\Object\StrictObject;
 
-class Exceptionality extends SelfTypedStrictStringEnum
+/**
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
+class Exceptionality extends StrictObject
 {
-    const EXCEPTIONALITY = 'exceptionality';
-
     /**
-     * @return Exceptionality
-     */
-    public static function getIt()
-    {
-        return static::getEnum(static::getKind());
-    }
-
-    /**
-     * @return string
-     */
-    public static function getKind()
-    {
-        if (static::class === __CLASS__) {
-            throw new \LogicException(
-                'The base exceptionality class ' . __CLASS__ . ' has no kind. It is just a base for others.'
-            );
-        }
-
-        return static::getTypeName();
-    }
-
-    /**
-     * All races can be annotated just as "race" type.
-     * The specific race will be build here, distinguished by the race and subrace code.
-     * Warning - each specific race has to be registered as a Doctrine type,
-     * @see StrictStringEnum::registerSelf()
+     * @var integer
      *
-     * @param string $kind
-     *
-     * @return Exceptionality
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
-    protected static function createByValue($kind)
-    {
-        $exceptionality = parent::createByValue($kind);
-        /** @var $exceptionality Exceptionality */
-        if ($exceptionality::getKind() !== $kind) {
-            throw new \LogicException(
-                'Given exceptionality type ' . var_export($kind, true) .
-                ' results into exceptionality ' . get_class($exceptionality) . ' with type ' . var_export($exceptionality::getKind(), true) . '.'
-            );
-        }
+    private $id;
 
-        return $exceptionality;
+    /**
+     * @var ExceptionalityChoice
+     *
+     * @ORM\Column(type="exceptionality_choice")
+     */
+    private $exceptionalityChoice;
+
+    /**
+     * @var ExceptionalityChoice
+     *
+     * @ORM\Column(type="exceptionality_kind")
+     */
+    private $exceptionalityKind;
+
+    public function __construct(
+        ExceptionalityChoice $exceptionalityChoice,
+        ExceptionalityKind $exceptionalityKind
+    )
+    {
+        $this->exceptionalityChoice = $exceptionalityChoice;
+        $this->exceptionalityKind = $exceptionalityKind;
     }
 
     /**
-     * @param string $type
-     *
-     * @return string
+     * @return int
      */
-    protected static function getEnumClass($type)
+    public function getId()
     {
-        $specificExceptionalityClass = parent::getEnumClass($type);
-        if ($specificExceptionalityClass === __CLASS__) {
-            throw new \LogicException(
-                "Given exceptionality type {$type} is not paired with specific exceptionality class"
-            );
-        }
-
-        return $specificExceptionalityClass;
+        return $this->id;
     }
 
+    /**
+     * @return ExceptionalityChoice
+     */
+    public function getExceptionalityChoice()
+    {
+        return $this->exceptionalityChoice;
+    }
+
+    /**
+     * @return ExceptionalityKind
+     */
+    public function getExceptionalityKind()
+    {
+        return $this->exceptionalityKind;
+    }
 }
