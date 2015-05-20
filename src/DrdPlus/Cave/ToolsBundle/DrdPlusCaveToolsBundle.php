cf41d31627bf8b8be238edc618bee2500a782250
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-05-20 07:37:10 +0200

    fix: Removed enums are not loaded

--- src/DrdPlus/Cave/ToolsBundle/DrdPlusCaveToolsBundle.php
+++ src/DrdPlus/Cave/ToolsBundle/DrdPlusCaveToolsBundle.php
@@ -1,21 +1,9 @@
 <?php
-
 namespace DrdPlus\Cave\ToolsBundle;
 
-use DrdPlus\Cave\ToolsBundle\Enum\DiceRoll1d6;
-use DrdPlus\Cave\ToolsBundle\Enum\DiceRoll2d6Plus;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DrdPlusCaveToolsBundle extends Bundle
 {
-    public function boot()
-    {
-        $this->registerEnums();
-    }
 
-    private function registerEnums()
-    {
-        DiceRoll1d6::registerSelf();
-        DiceRoll2d6Plus::registerSelf();
-    }
 }
