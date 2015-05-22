cd1e6ff147ca319b3fb10f0696cba0e645c7e8a8
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-05-22 08:22:50 +0200

    Combination exceptionality kind is covered by tests

---
+++ src/DrdPlus/Cave/UnitBundle/Tests/Entity/Attributes/Exceptionalities/Kinds/CombinationTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace DrdPlus\Cave\UnitBundle\Entity\Attributes\Exceptionalities\Kinds;
+
+use DrdPlus\Cave\UnitBundle\Tests\Entity\Attributes\Exceptionalities\Kinds\AbstractTestOfKind;
+
+class CombinationTest extends AbstractTestOfKind
+{
+
+}
