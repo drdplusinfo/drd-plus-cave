b2e2ac904b73b6a16879533bb8623a9d63036732
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-06-05 17:24:14 +0200

    Tests maximal nesting level limited

--- _tests_bootstrap.php
+++ _tests_bootstrap.php
@@ -3,5 +3,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 error_reporting(-1);
 ini_set('display_errors', true);
 
+ini_set('xdebug.max_nesting_level', 50);
+
 $drdPlusCaveUnitBundle = new \DrdPlus\Cave\UnitBundle\DrdPlusCaveUnitBundle();
 $drdPlusCaveUnitBundle->boot();
