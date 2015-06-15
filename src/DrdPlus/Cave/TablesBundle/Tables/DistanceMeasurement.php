9acd6d886825815d8a805ea3505eebc9ea4fdb3a
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-06-15 08:59:21 +0200

    Distance table added

---
+++ src/DrdPlus/Cave/TablesBundle/Tables/DistanceMeasurement.php
@@ -0,0 +1,69 @@
+<?php
+namespace DrdPlus\Cave\TablesBundle\Tables;
+
+class DistanceMeasurement implements MeasurementInterface
+{
+    const M = 'm';
+    const KM = 'km';
+
+    /**
+     * @var float
+     */
+    private $value;
+    /**
+     * @var string
+     */
+    private $unit;
+
+    public function __constructor($value, $unit)
+    {
+        $this->value = $value;
+        $this->checkUnit($unit);
+        $this->unit = $unit;
+    }
+
+    private function checkUnit($unit)
+    {
+        switch ($unit) {
+            case self::M :
+            case self::KM :
+                return;
+            default :
+                throw new \LogicException('Unknown unit ' . var_export($unit, true));
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getUnit()
+    {
+        return $this->unit;
+    }
+
+    /**
+     * @return float
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function toMeters()
+    {
+        if ($this->unit === self::M) {
+            return $this->value;
+        }
+
+        return $this->value * 1000;
+    }
+
+    public function toKilometers()
+    {
+        if ($this->unit === self::KM) {
+            return $this->value;
+        }
+
+        return $this->value / 1000;
+    }
+}
---
+++ src/DrdPlus/Cave/TablesBundle/Tables/DistanceTable.php
@@ -0,0 +1,67 @@
+<?php
+namespace DrdPlus\Cave\TablesBundle\Tables;
+
+/**
+ * PPH page 162, top
+ * @method DistanceMeasurement toMeasurement(int $bonus)
+ */
+class DistanceTable extends AbstractTable
+{
+
+    protected function getDataFileName()
+    {
+        return __DIR__ . '/data/distance.csv';
+    }
+
+    protected function getExpectedDataHeader()
+    {
+        return [DistanceMeasurement::M, DistanceMeasurement::KM];
+    }
+
+    protected function getExpectedDataRowsCount()
+    {
+        return 160;
+    }
+
+    /**
+     * @param int $bonus
+     *
+     * @return float
+     */
+    public function toMeters($bonus)
+    {
+        return $this->toMeasurement($bonus)->toMeters();
+    }
+
+    /**
+     * @param int $bonus
+     *
+     * @return float
+     */
+    public function toKilometers($bonus)
+    {
+        return $this->toMeasurement($bonus)->toKilometers();
+    }
+
+    public function kilometersToBonus($km)
+    {
+        return $this->toBonus(new DistanceMeasurement($km, DistanceMeasurement::KM));
+    }
+
+    public function metersToBonus($m)
+    {
+        return $this->toBonus(new DistanceMeasurement($m, DistanceMeasurement::M));
+    }
+
+    /**
+     * @param float $value
+     * @param string $unit
+     *
+     * @return DistanceMeasurement
+     */
+    protected function convertToMeasurement($value, $unit)
+    {
+        return new DistanceMeasurement($value, $unit);
+    }
+
+}
