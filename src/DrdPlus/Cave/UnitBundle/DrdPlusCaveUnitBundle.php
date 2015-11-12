4cecaadbd2ed9175fb26db34918b75a0e9ac91f2
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-11-12 10:43:19 +0100

    Temporary removed registering of enums

--- src/DrdPlus/Cave/UnitBundle/DrdPlusCaveUnitBundle.php
+++ src/DrdPlus/Cave/UnitBundle/DrdPlusCaveUnitBundle.php
@@ -1,24 +1,6 @@
 <?php
 namespace DrdPlus\Cave\UnitBundle;
 
-use DrdPlus\Cave\UnitBundle\Person\Attributes\Exceptionalities\Choices\ExceptionalityChoice;
-use DrdPlus\Cave\UnitBundle\Person\Attributes\Exceptionalities\Fates\AbstractFateEntity;
-use DrdPlus\Cave\UnitBundle\Person\Attributes\Experiences\ExperiencesType;
-use DrdPlus\Cave\UnitBundle\Person\Attributes\Name;
-use DrdPlus\Cave\UnitBundle\Person\Background\BackgroundSkills;
-use DrdPlus\Cave\UnitBundle\Person\Background\BelongingsValue;
-use DrdPlus\Cave\UnitBundle\Person\Background\Heritage;
-use DrdPlus\Cave\UnitBundle\Person\Background\Parts\BackgroundPointsType;
-use DrdPlus\Cave\UnitBundle\Person\ProfessionLevels\LevelRank;
-use DrdPlus\Cave\UnitBundle\Person\Races\Gender;
-use DrdPlus\Cave\UnitBundle\Person\Races\Race;
-use DrdPlus\Properties\Agility;
-use DrdPlus\Properties\Body\WeightInKg;
-use DrdPlus\Properties\Charisma;
-use DrdPlus\Properties\Intelligence;
-use DrdPlus\Properties\Knack;
-use DrdPlus\Properties\Strength;
-use DrdPlus\Properties\Will;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DrdPlusCaveUnitBundle extends Bundle
@@ -30,30 +12,6 @@ class DrdPlusCaveUnitBundle extends Bundle
 
     private function registerEnums()
     {
-        Name::registerSelf();
-        // only the generic race has to be registered, sub-races are registered automatically
-        Race::registerSelf();
-        // only the generic gender has to be registered, sub-race genders are registered automatically
-        Gender::registerSelf();
-        // only the generic choice has to be registered, specific choices are registered automatically
-        ExceptionalityChoice::registerSelf();
-        // only the generic kind has to be registered, specific kinds are registered automatically
-        AbstractFateEntity::registerSelf();
-
-        Strength::registerSelf();
-        Agility::registerSelf();
-        Knack::registerSelf();
-        Will::registerSelf();
-        Intelligence::registerSelf();
-        Charisma::registerSelf();
-//        WeightInKg::registerSelf();
-
-        LevelRank::registerSelf();
-        ExperiencesType::registerSelf();
-
-        BackgroundPointsType::registerSelf();
-        BackgroundSkills::registerSelf();
-        BelongingsValue::registerSelf();
-        Heritage::registerSelf();
+        // TODO
     }
 }
