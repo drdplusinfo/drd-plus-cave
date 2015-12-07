0f4e2aa88ef5575c31e396c8a191ab3bb56a362b
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-12-07 13:59:33 +0100

    Removed unused attribute-specific exceptions

--- src/DrdPlus/Cave/UnitBundle/Person/Attributes/Exceptions/Exception.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace DrdPlus\Cave\UnitBundle\Person\Attributes\Exceptions;
-
-interface Exception extends \DrdPlus\Cave\UnitBundle\Person\Exceptions\Exception {
-
-}
--- src/DrdPlus/Cave/UnitBundle/Person/Attributes/Exceptions/Logic.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace DrdPlus\Cave\UnitBundle\Person\Attributes\Exceptions;
-
-interface Logic extends Exception, \DrdPlus\Cave\UnitBundle\Person\Exceptions\Logic
-{
-
-}
