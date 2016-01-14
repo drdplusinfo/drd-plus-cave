ef4d8d6a1d8c32d3b1625cd9a36d156cf54bdb93
Jaroslav Tyc <mail@jaroslavtyc.com>
2016-01-14 14:28:31 +0100

    Tests can be run from any dir

--- _tests_bootstrap.php
+++ _tests_bootstrap.php
@@ -2,8 +2,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 error_reporting(-1);
 ini_set('display_errors', true);
-
 ini_set('xdebug.max_nesting_level', 100);
+$_SERVER['KERNEL_DIR'] = __DIR__ . '/app';
 
 $drdPlusCaveUnitBundle = new \DrdPlus\Cave\UnitBundle\DrdPlusCaveUnitBundle();
 $drdPlusCaveUnitBundle->boot();
