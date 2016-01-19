4de1d93c64739a174968a6e98ecb6b5edb45f289
Jaroslav Tyc <mail@jaroslavtyc.com>
2016-01-19 15:18:58 +0100

    Cache problem resolved by ACL

--- web/app.php
+++ web/app.php
@@ -1,9 +1,10 @@
 <?php
-
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+//umask(0002);
+
+$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
@@ -14,7 +15,7 @@ $loader->unregister();
 $apcLoader->register(true);
 */
 
-require_once __DIR__.'/../app/AppKernel.php';
+require_once __DIR__ . '/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 require_once __DIR__ . '/../app/_bootstrap.php';
 
