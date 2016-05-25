66e7c64377438ada9c79116a9a204136951cd618
Jaroslav Tyc <mail@jaroslavtyc.com>
2016-05-25 18:25:54 +0200

    Tests bootstrap minor code beautify

--- _tests_bootstrap.php
+++ _tests_bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+
 error_reporting(-1);
 ini_set('display_errors', true);
-ini_set('xdebug.max_nesting_level', 100);
+ini_set('xdebug.max_nesting_level', 200);
+
 $_SERVER['KERNEL_DIR'] = __DIR__ . '/app';
 
 $drdPlusCaveUnitBundle = new \DrdPlus\Cave\UnitBundle\DrdPlusCaveUnitBundle();
